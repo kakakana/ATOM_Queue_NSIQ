56e654898b93f9e33c4c7879c77bb6c0e87ee0c3
weawen <weawen@ntels.com>
2015-12-22 14:29:27 +0900

    Modified CMRTMain 20151222.14:30

--- MRT_SRC/CMRTMain.cpp
+++ MRT_SRC/CMRTMain.cpp
@@ -4,12 +4,13 @@
  */
 
 // Include Standard Header
-#include <CMRTMain.hpp>
+#include "CMRTMain.hpp"
 #include <stdio.h>
 #include <stdint.h>
 #include <errno.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 
 // Include Definition For DPDK Envirionment Variables and Functions
 #include <rte_config.h>
@@ -36,13 +37,23 @@
 //! Constructor
 /*!
  * \brief Constructor For CMRTMain Class
- *
- * Init Variables
+ * \details Init Variables 
+ * \param a_strLogPath is Log file path if NULL, default stdout
  */
-CMRTMain::CMRTMain()
+CMRTMain::CMRTMain(char *a_strLogPath)
 {
+	//Copy Log path
+	memset(m_strLogPath, 0x00, sizeof(m_strLogPath));
 
+	//a_strLogPath Default is NULL
+	if(a_strLogPath)
+	{
+		snprintf(m_strLogPath, sizeof(m_strLogPath), "%s",  a_strLogPath);
+	}
 
+	//Init Variables
+	m_pstRingInfo = NULL;
+	m_pstMemPool = NULL;
 
 }
 
@@ -50,7 +61,7 @@ CMRTMain::CMRTMain()
 /*!
  * \brief Destructor For CMRTMain Class
  *
- * Init Variables and Delete Memory
+ * \details Init Variables and Delete Memory
  */
 CMRTMain::~CMRTMain()
 {
@@ -59,13 +70,118 @@ CMRTMain::~CMRTMain()
 }
 
 
+//! Initialize
+/*!
+ * \brief Init Variables and DPDK Library
+ * \param NULL
+ * \return Succ 0, Fail -1
+ */
+int CMRTMain::Initialize()
+{
+	int ret = 0;
+	
+	//Set Process Type (primary)
+	rte_eal_set_proc_type(RTE_PROC_PRIMARY);
+	//Init to DPDK Library	
+	ret = rte_eal_init(m_strLogPath);
+
+	//Failed
+	if(ret < 0)
+	{
+		RTE_LOG (INFO, EAL, "Cannot init MRT\n");
+		return -1;
+	}
+
+	//Ring Create For To Communicate to Client
+	/* [Parameters]
+	 * name, Ring Count, Memory Socket ID, Flags
+	 */
+	m_pstRingInfo = rte_ring_create((char*)DEF_RING_INFO_NAME, DEF_RING_SIZE, SOCKET_ID_ANY, 0);	
+	if(m_pstRingInfo == NULL)
+	{
+		RTE_LOG (INFO, EAL, "Cannot Create ring\n");
+		return -1;
+	}
+
+	/* [Parameters]
+	 * name, elem Count, elem Size, cache Size, private Data Size,
+	 * Memory Pool Init Func, Memory Pool Init func Argument
+	 * element Init Func, element Init Func Argument
+	 * Memory Socket ID, Flags
+	 */
+	m_pstMemPool = rte_mempool_create(DEF_MEMORY_POOL_NAME, DEF_MBUF_COUNT,
+										DEF_MBUF_SIZE, DEF_MBUF_CACHE_SIZE,
+										sizeof(struct rte_pktmbuf_pool_private), rte_pktmbuf_pool_init,
+										NULL, rte_pktmbuf_init, NULL, SOCKET_ID_NAY, 0); 
+	if(m_pstMemPool)
+	{
+		RTE_LOG (INFO, EAL, "Cannot Create Memory Pool\n");
+		return -1;
+	}
+
+	return 0;
+}
+
 //! Create the Ring
 /*! 
+ * \brief Create the Ring for enqeueing elements
  * \param a_strName is name for ring
  * \return Succ 0, Fail -1
  */
-int CreateRing(char *a_strName)
+int CMRTMain::CreateRing(char *a_strName)
 {
 
 	return 0;
 }
+
+
+//! Create Mempool 
+/*!
+ * \brief Create Mempool For Ring info to use in Application
+ * \param NULL
+ * \return Succ 0, Fail -1
+ */
+int CMRTMain::CreateMempool()
+{
+
+
+	return 0;
+}
+
+//! Run Process
+/*!
+ * \brief Run Process for CMRT Process
+ * \param NULL
+ * \return Succ 0, Fail -1
+ */
+int CMRTMain::Run()
+{
+
+	return 0;
+}
+
+//! Main Function
+/*!
+ * \brief main Function For MRT Process
+ * \param Argument count, String Array of Arguments
+ * \return Succ 0, Fail -1
+ */
+int main(int argc, char *args[])
+{
+	CMRTMain *m_pclsCMRT = NULL;
+
+	m_pclsCMRT = new CMRTMain();
+
+	//Failed
+	if(m_pclsCMRT == NULL)
+		return -1;
+
+
+	m_pclsCMRT->Initialize();
+	m_pclsCMRT->Run();
+
+	delete m_pclsCMRT;
+
+	return 0;
+}
+
--- MRT_SRC/CMRTMain.hpp
+++ MRT_SRC/CMRTMain.hpp
@@ -5,6 +5,53 @@
 #ifndef _MRT_MAIN_H_
 #define _MRT_MAIN_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
+//! Define Memory Buffer 265 byte
+#define DEF_MEM_BUF_256		256
+
+//! Define Default Ring Size, (262,144)
+/*!
+ * Must be Power of 2
+ */
+#define DEF_RING_SIZE	0x40000  
+
+//! Define Ring Name
+/*!
+ * Ring Name For to Communicate to Application
+ */
+#define DEF_RING_INFO_NAME	"RING_INFO"  
+
+//! Define Memory Pool Name
+/*!
+ * Memory Pool Name For to Use in Application
+ */
+#define DEF_MEMORY_POOL_NAME	"BASE_MEMORY_POOL"  
+
+//! Define Memory Buffer Elements Count
+/*!
+ * To Use in Memory Pool Create Function
+ * Power of 2 - 1
+ */
+#define DEF_MBUF_COUNT	( 1 << 18 ) - 1  
+
+//! Define Memory Buffer Cache Size
+#define DEF_MBUF_CACHE_SIZE 512
+
+//! Define Memory Buffer Over Head Size
+#define DEF_MBUF_OVERHEAD (sizeof(struct rte_mbuf) + RTE_PKTMBUF_HEADROOM)
+
+//! Define Memory Buffer Data Size
+#define DEF_MBUF_DATA_SIZE 2048
+
+//! Define Memory Buffer Elements Size
+/*!
+ * To Use in Memory Pool Create Function
+ */
+#define DEF_MBUF_SIZE ( DEF_MBUF_DATA_SIZE + DEF_MBUF_OVERHEAD )
+
+
 /*!
  * \class CMRTMain
  * \brief CMRT Main Class
@@ -13,18 +60,28 @@ class CMRTMain
 {
 	public:
 		//! Constructor.
-		CMRTMain();
+		CMRTMain(char *a_strLogPath = NULL);
 		//! Destructor.
 		~CMRTMain();
 
-	private:
-		//! Create the Ring
-		/*!
-		 * \param a_strName is name for ring
-		 * \return Succ 0, Fail -1
-		 */
-		int CreateRing(char *a_strName);
+		//! Init Class
+		int Initialize();
+		//! Run Process
+		int Run();
 
+	private:
+		//! Log Path 
+		char m_strLogPath[DEF_MEM_BUF_256];
+		//! Ring For to Communicate to Application
+		struct rte_ring *m_pstRingInfo;
+		//! Memory Pool For using in Application 
+		struct rte_mempool *m_pstMemPool;
+		//! Create the Ring
+		int CreateRing(char *a_strName);
+		//! Create Mempool
+		int CreateMempool();
+
+		
 };
 
 #endif
